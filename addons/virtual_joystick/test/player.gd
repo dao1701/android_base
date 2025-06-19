649a24540b591e07761b485cd00e08d6d6d62f34
daotv <daotv@vng.com.vn>
2025-06-19 11:46:42 +0700

    Refactor player movement and joystick handling for improved clarity and consistency

--- addons/virtual_joystick/test/player.gd
+++ addons/virtual_joystick/test/player.gd
@@ -9,15 +9,15 @@ extends Sprite2D
 var move_vector := Vector2.ZERO
 
 func _process(delta: float) -> void:
-	## Movement using the joystick output:
+    ## Movement using the joystick output:
 #	if joystick_left and joystick_left.is_pressed:
 #		position += joystick_left.output * speed * delta
-	
-	## Movement using Input functions:
-	move_vector = Vector2.ZERO
-	move_vector = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	position += move_vector * speed * delta
-	
-	# Rotation:
-	if joystick_right and joystick_right.is_pressed:
-		rotation = joystick_right.output.angle()
+    
+    ## Movement using Input functions:
+    move_vector = Vector2.ZERO
+    move_vector = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+    position += move_vector * speed * delta
+    
+    # Rotation:
+    if joystick_right and joystick_right.is_pressed:
+        rotation = joystick_right.output.angle()
